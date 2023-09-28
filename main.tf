9904e9e58884ab2113f6b326881ff9d4360a22c3
Seul-gi Baggelaar <seul.baggelaar@protonmail.com>
2023-09-28 19:30:46 +0200

    shell-scripts-added-docker-nginx

--- main.tf
+++ main.tf
@@ -142,6 +142,14 @@ resource "aws_security_group" "myapp-security-group" {
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
+    user_data = <<E0F
+                    #!/bin/bash
+                    sudo yum update -y && sudo yum install -y docker
+                    sudo systemctl start docker
+                    sudo usermod -aG docker ec2-user
+                    docker run -p 8080:80 nginx
+                  E0F
+
     tags = {
       name = "${var.env_prefix}-server"
     }
