9cbf554688a61a2250196ac38599693424b26d18
Seul-gi Baggelaar <seul.baggelaar@protonmail.com>
2023-09-28 18:55:16 +0200

    my-aws-vpc-ec2-infrastructure

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 variable allowed_ips {}
+variable instance_type {}
+variable public_key_location {
+  
+}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -112,11 +116,33 @@ resource "aws_security_group" "myapp-security-group" {
     }
   }
 
+  resource "aws_key_pair" "ssh-key" {
+      key_name = "server-key"
+      public_key = "${file(var.public_key_location)}"
+
+  }
+
+    output "ec2_public_ip"  {
+      value = aws_instance.myapp-server.public_ip
+    
+  }
+
   resource "aws_instance" "myapp-server" {
     # we actually want this ID not directly from Amazon, but through TerraForm so that it can change dynamically over time
     # ami = "ami-0fc067f03ad87bb64"
     # see the above block data
 
     ami = data.aws_ami.amazon-latest-linux-os.id
-    
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-security-group.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+
+    tags = {
+      name = "${var.env_prefix}-server"
+    }
   }
